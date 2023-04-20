444431aa2ce55ca0647900e2372b4285c75fd0f1
Chandler <underwoo16@github.com>
2023-04-20 22:32:27 +0000

    Added workflow to run action in this repository

--- dist/index.js
+++ dist/index.js
@@ -56,9 +56,9 @@ function addToTasklist() {
             return;
         // This should be a token with access to your repository scoped in as a secret.
         // The YML workflow will need to set myToken with the GitHub Secret Token
-        // myToken: ${{ secrets.GITHUB_TOKEN }}
+        // github-token: ${{ secrets.GITHUB_TOKEN }}
         // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-        const myToken = core.getInput('myToken');
+        const myToken = core.getInput('github-token');
         const octokit = github.getOctokit(myToken);
         // fetch tracking issues for this milestone
         const trackingIssues = yield octokit.rest.issues.listForRepo({
--- src/add-to-tasklist.ts
+++ src/add-to-tasklist.ts
@@ -15,9 +15,9 @@ export async function addToTasklist(): Promise<void> {
 
   // This should be a token with access to your repository scoped in as a secret.
   // The YML workflow will need to set myToken with the GitHub Secret Token
-  // myToken: ${{ secrets.GITHUB_TOKEN }}
+  // github-token: ${{ secrets.GITHUB_TOKEN }}
   // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-  const myToken = core.getInput('myToken')
+  const myToken = core.getInput('github-token')
 
   const octokit = github.getOctokit(myToken)
 
